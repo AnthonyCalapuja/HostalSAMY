b437f744c5bd76e0d7e5eeb75b39151cb5a4fe32
Anthony <anthonycalapuja@gmail.com>
2022-05-05 23:17:09 -0500

    Prueba 1

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
-HOla
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostal SAMY </title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+
+
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
+
+</body>
+</html>
